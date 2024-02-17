a5efbe9f972eb0d0250ec017ae6c38b1c0777d82
Emrul <emrul.hoque@gmail.com>
2024-02-17 23:11:06 +0600

    "Articles app"

--- newspaper_project/newspaper_project/settings.py
+++ newspaper_project/newspaper_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "articles.apps.ArticlesConfig",
     "users.apps.UsersConfig",
     "pages.apps.PagesConfig",
     "crispy_forms",
