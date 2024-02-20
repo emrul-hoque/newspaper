8ae4383bc6006e27511d5dada18a7a2f83cb830d
Emrul <emrul.hoque@gmail.com>
2024-02-20 22:08:45 +0600

    "update"

--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Article
 
-# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+    
+
+
